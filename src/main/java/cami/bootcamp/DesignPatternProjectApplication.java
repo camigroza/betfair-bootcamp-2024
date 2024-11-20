ef39c04ffdc245b17c6ad8d087a91adbcdca1406
Cami Groza <cami.groza.1606@gmail.com>
2024-11-20 20:43:52 +0200

    Connection to my sql database established

---
+++ src/main/java/cami/bootcamp/DesignPatternProjectApplication.java
@@ -0,0 +1,13 @@
+package cami.bootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignPatternProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignPatternProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cami/bootcamp/DesignPatternProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package cami.bootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
